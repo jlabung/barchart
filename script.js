c50b54a0ebc456770f1a7d079585206f7f43b53a
Juga limbu <labungjugal1@gmail.com>
2020-02-13 21:52:48 +0100

    made the bar chart work

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 "use strict";
+window.addEventListener("DOMContentLoaded", arrayExceeds);
+let myArray = [];
+let counter = 0;
+let timeOut = 200;
+
+function arrayExceeds() {
+  if (counter <= myArray.length) {
+    addSomething();
+  } else {
+    addSomething();
+    myArray.pop();
+  }
+}
+function addSomething() {
+  myArray.unshift(Math.floor(Math.random() * 32));
+  counter++;
+  console.log(myArray);
+  setTimeout(arrayExceeds, timeOut);
+  setHeight();
+}
+
+function setHeight() {
+  //   document.querySelectorAll(".bar").forEach((bar, i) => {
+  // bar.style.setProperty("--height", myArray[i]);
+
+  //});
+
+  for (let i = 0; i < myArray.length; i++) {
+    let bar;
+    bar = document.querySelector(`#bar > div:nth-child(${i + 1})`);
+    bar.style.setProperty("--height", myArray[i]);
+  }
+}
